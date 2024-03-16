c39278d9fe60c6ba06ddc3173806a2f92aa6d781
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-03-16 11:40:20 +0530

    First commit

--- MinTimeSpentByUsers/Project6/src/main/java/webclicks5/minTimeMapper.java
+++ MinTimeSpentByUsers/Project6/src/main/java/webclicks5/minTimeMapper.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class minTimeMapper extends Mapper<LongWritable, Text,Text, IntWritable>  {
 
 	@Override
+        // should throw IOexception
 	protected void map(LongWritable key, Text value, Context context) throws IOException , InterruptedException{
 		String[] data = value.toString().split("\t");
 		String users = data[4];
